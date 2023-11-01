7d104fc9da862ff4be91fc17fdb1d641574a613a
Bruno <14274839-bcostaaa01@users.noreply.gitlab.com>
2023-11-01 20:28:53 +0000

    Add recursive directory walk class

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import os
 import argparse
 
+class RecursiveWalk:
+    def __init__(self):
+        self.path = ''
+        self.recursive_walk_list = []
+
+    @staticmethod
+    def get_args():
+        parser = argparse.ArgumentParser(description='Scan files in a directory')
+        parser.add_argument('-p', '--path', metavar='path', type=str, help='Path to directory')
+        return parser.parse_args()
+
+    def scan_path(self):
+        for root, dirs, files in os.walk(self.path):
+            for entry in files:
+                self.recursive_walk_list.append(os.path.join(root, entry))
+
+    def main(self):
+        args = self.get_args()
+        self.path = args.path
+        self.scan_path()
+        print(self.recursive_walk_list)
+
 class DirectoryWalk:
     def __init__(self):
         self.path = ''
@@ -22,5 +44,8 @@ class DirectoryWalk:
         self.scan_path()
 
 if __name__ == '__main__':
-    walk_instance = DirectoryWalk()
+    walk_instance = RecursiveWalk()
     walk_instance.main()
+    
+    # recursive_directory_walk_instance = DirectoryWalk()
+    # recursive_directory_walk_instance.main()
